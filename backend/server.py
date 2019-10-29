3c014a05b6de0ff9ceb05db810239568b393dc14
karkay <kentkfeng@gmail.com>
2019-10-29 14:41:31 -0400

    converted event to json and added event type detection for proper updating

--- backend/server.py
+++ backend/server.py
@@ -13,7 +13,8 @@ w3=Web3(HTTPProvider(NODE_ADDRESS))
 apiURL="https://api.etherscan.io/api?"
 apiKey="S4TBCHFSXQFMW43VZKB436H91PXFU4CPUN"
 
-
+# congo = await Congo.deployed()
+# updateItemTwo = await congo.updateListing(2,42,70,"details for item 2","item twoo!")
 
 with open("../contracts/contracts/build/contracts/Congo.json") as f:
     info_json = json.load(f)
@@ -25,24 +26,59 @@ contract=w3.eth.contract(address=contract_address,abi=congo_abi)
 accounts=w3.eth.accounts
 
 
+def convertEvent(event):
+    jsonStr = ""
+    for key in event.args:
+        keyVal = '"' + key + '"'+ ": " + '"' + str(event.args[key]) + '"' + ','
+        jsonStr += keyVal
+    jsonStr = jsonStr[:-1]
+    jsonStr = "{" + jsonStr + "}"
+    # print(jsonStr)
+    # print(type(jsonStr))
+    return json.loads(jsonStr)
+
+
 def print_event(event):
-    # print(event)
-    eventJson = json.load(w3.toJSON(event))
-    print(eventJson)
-    print(eventJson['args'])
-    # receipt=w3.eth.waitForTransactionReceipt(event['transactionHash'])
-    # print("receipt:"+receipt)
-    # result=contract.events.greeting.processReceipt(receipt)
-    # print("result:"+result)
+    res = convertEvent(event)
+    print(res)
+    print(type(res))
 
-    # print(result[0]['args'])
+def putNewProduct(event):
+    print("newly listed prod")
+    print_event(event)
+
+def updateListing(event):
+    print("update listed prod")
+    print_event(event)
+
+def putNewOrder(event):
+    print("new order")
+    print_event(event)
+
+def updateOrder(event):
+    print("updated order")
+    print_event(event)
 
 
-def log_loop(event_filter,poll_interval):
+def log_loop(event_filter,poll_interval,event_type):
+    print("started worker thread!")
     while True:
-        for event in event_filter.get_new_entries():
-            print_event(event)
-        time.sleep(poll_interval)
+        if(event_type == "productListed"):
+            for event in event_filter.get_new_entries():
+                putNewProduct(event)
+            time.sleep(poll_interval)
+        elif(event_type == "listingUpdated"):
+            for event in event_filter.get_new_entries():
+                updateListing(event)
+            time.sleep(poll_interval)
+        elif(event_type == "orderCreated"):
+            for event in event_filter.get_new_entries():
+                putNewOrder(event)
+            time.sleep(poll_interval)
+        elif(event_type == "orderUpdated"):
+            for event in event_filter.get_new_entries():
+                updateOrder(event)
+            time.sleep(poll_interval)
 
 def queryNetwork(address,moduleType,actionType):
     parameters={
@@ -67,6 +103,15 @@ def search(itemText):
 
 if __name__ == '__main__':
     productListed_filter=contract.events.productListed.createFilter(fromBlock='latest')
-    worker=Thread(target=log_loop,args=(productListed_filter,5),daemon=True)
-    worker.start()
+    listingUpdated_filter=contract.events.listingUpdated.createFilter(fromBlock='latest')
+    orderCreated_filter=contract.events.orderCreated.createFilter(fromBlock='latest')
+    orderUpdated_filter=contract.events.orderUpdated.createFilter(fromBlock='latest')
+    filters = {"productListed" :productListed_filter,
+               "listingUpdated":listingUpdated_filter,
+               "orderCreated":orderCreated_filter,  
+               "orderUpdated":orderUpdated_filter
+               }
+    for key in filters:
+        worker=Thread(target=log_loop,args=(filters[key],5,key),daemon=True)
+        worker.start()
     app.run()
