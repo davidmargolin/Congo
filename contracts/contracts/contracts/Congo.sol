d7af5d51d4358030b5d10ee412a403f20117f063
karkay <kentkfeng@gmail.com>
2019-11-11 20:35:12 -0500

    implemented creating and updating orders and included some manual truffle tests.

--- contracts/contracts/contracts/Congo.sol
+++ contracts/contracts/contracts/Congo.sol
@@ -3,13 +3,27 @@ pragma solidity ^0.5.0;
 contract Congo {
 	string public name;
 	uint public productCount;
+	uint public orderCount;
 	enum State {Listed,Processing,Shipped,Complete,Exception}
 	mapping(uint => Product) public products;
+	mapping(uint => Order) public orders;
 
+	struct Order{
+		uint id;
+		uint quantity;
+		uint prodID;
+		uint256 total;
+		string prodName;
+		address payable buyerAddress;
+		address payable sellerAddress;
+		string notes;
+		State orderStatus;
+
+	}
 	struct Product{
 		uint id;
 		uint quantity;
-		uint price;
+		uint256 price;
 		string details;
 		string name;
 		address payable owner;
@@ -18,7 +32,7 @@ contract Congo {
 	event listingCreated(
 		uint id,
 		uint quantity,
-		uint price,
+		uint256 price,
 		string details,
 		string name,
 		address payable owner
@@ -27,26 +41,27 @@ contract Congo {
 	event listingUpdated(
 		uint id,
 		uint quantity,
-		uint price,
+		uint256 price,
 		string details,
 		string name,
 		address payable owner
 	);
 	//event to be emitted when an order is updated.
 	event orderUpdated(
-		uint id,
-		uint quantity,
-		uint price,
-		string details,
-		string name,
-		address payable owner
+		uint orderID,
+		State nextState
 	);
 	//emit that an order has been created between the two parties
 	event orderCreated(
-		uint id,
+		uint orderID,
 		uint quantity,
-		address payable buyer,
-		address payable seller
+		uint prodID,
+		uint256 total,
+		string prodName,
+		address payable buyerAddress,
+		address payable sellerAddress,
+		string notes,
+		State orderStatus
 	);
 	constructor() public {
 		name = "Congo Exchange";
@@ -54,7 +69,7 @@ contract Congo {
 	//exposed function that lists a product.
 	function createListing(
 		uint _quantity,
-		uint _price,
+		uint256 _price,
 		string memory _details,
 		string memory _name)
 	public {
@@ -89,7 +104,7 @@ contract Congo {
 	function updateListing(
 		uint _id,
 		uint _quantity,
-		uint _price,
+		uint256 _price,
 		string memory _details,
 		string memory _name)
 	public payable{
@@ -129,9 +144,10 @@ contract Congo {
 	}
 
 	//exposed function that allows user to purchase products
-	function purchaseProduct(
+	function createOrder(
 		uint _id,
-		uint _quantity
+		uint _quantity,
+		string memory notes
 	)
 	public payable{
 		//validate user's input
@@ -142,8 +158,9 @@ contract Congo {
 		Product memory _listing = products[_id];
 		address payable seller = _listing.owner;
 		address payable buyer = msg.sender;
-		uint orderTotal = _listing.price * _quantity;
-
+		uint quantityRemaining = _listing.quantity;
+		require(_quantity <= quantityRemaining,"Purchase Quantity exceeds Available Quantity");
+		uint256 orderTotal = _listing.price * _quantity;
 		//make sure buyer is not seller.
 		require(buyer != seller, "You cannot purchase your own products.");
 		//make sure payment is sufficient.
@@ -151,52 +168,57 @@ contract Congo {
 
 		//update the listing quantity.
 		_listing.quantity = _listing.quantity - _quantity;
+		products[_id] = _listing;
+		//create a new order!
+		orderCount++;
+		orders[orderCount] = Order(
+			orderCount,
+			_quantity,
+			_id,
+			orderTotal,
+			_listing.name,
+			buyer,
+			seller,
+			notes,
+			State.Listed
+		);
 
 		//transfer value.
 		address(seller).transfer(msg.value);
-
 		//broadcast to listeners that an existing product has been updated.
 		emit orderCreated(
-			productCount,
+			orderCount,
 			_quantity,
+			_id,
+			orderTotal,
+			_listing.name,
 			buyer,
-			seller
+			seller,
+			notes,
+			State.Listed
 		);
 	}
-	** UNFINISHED ** 
 	//exposed function that allows seller to update orders
 	function updateOrder(
 		uint _id,
-		uint _quantity
+		uint nextState
 	)
 	public payable{
 		//validate user's input
-		require(_quantity > 0, "Product Quantity must be greater than zero");
-		require(_id <= productCount && _id > 0, "product id is invalid.");
+		require(_id <= orderCount && _id > 0, "order id is invalid.");
+		require(uint(State.Exception) >= nextState,"invalid input state. >");
+		//get the order.
+		Order memory _order = orders[_id];
+		//check if caller has permission to edit
+		require(msg.sender == _order.sellerAddress,"Only the seller can modify the order status.");
+		_order.orderStatus = State(nextState);
+		//update order struct in mapping
+		orders[_id] = _order;
 
-		//get the product.
-		Product memory _listing = products[_id];
-		address payable seller = _listing.owner;
-		address payable buyer = msg.sender;
-		uint orderTotal = _listing.price * _quantity;
-
-		//make sure buyer is not seller.
-		require(buyer != seller, "You cannot purchase your own products.");
-		//make sure payment is sufficient.
-		require(msg.value >= orderTotal, "Not enough to cover the total cost.");
-
-		//update the listing quantity.
-		_listing.quantity = _listing.quantity - _quantity;
-
-		//transfer value.
-		address(seller).transfer(msg.value);
-
-		//broadcast to listeners that an existing product has been updated.
-		emit orderCreated(
-			productCount,
-			_quantity,
-			buyer,
-			seller
+		//broadcast to listeners that an existing order has been updated.
+		emit orderUpdated(
+			_id,
+			_order.orderStatus
 		);
 	}
 }
