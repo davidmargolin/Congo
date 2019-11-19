a4a94e5c44165c31e4a6791ddaff449df7ffb115
David Margolin <davidmargolin0305@gmail.com>
2019-11-19 14:28:40 -0500

    support opera browser

--- frontend/src/pages/ListingPage/index.jsx
+++ frontend/src/pages/ListingPage/index.jsx
@@ -10,13 +10,40 @@ const getListing = id =>
 
 const CONTRACT_ADDRESS = "0xD95F794BA7686bf0944b7Eb6fa7311BdeC762607";
 
+const makePurchase = (id, price, quantity, email) => {
+  if (window.ethereum) {
+    window.ethereum
+      .enable()
+      .then(() => {
+        const web3 = new Web3(window.ethereum);
+        const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+        web3.eth.getAccounts().then(accounts => {
+          const transaction = contract.methods.createOrder(
+            id,
+            quantity,
+            CONTRACT_ADDRESS,
+            email
+          );
+
+          transaction
+            .send({
+              from: web3.currentProvider.selectedAddress || accounts[0],
+              value: price
+            })
+            .then(response => console.log(response));
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+
 const ListingPage = () => {
   const { listingID } = useParams();
   const [listingData, setListingData] = useState(null);
-
   useEffect(() => {
     getListing(listingID).then(data => {
-      console.log(data);
       setListingData(data);
     });
   }, [listingID]);
@@ -51,30 +78,9 @@ const ListingPage = () => {
           </a>
           <p>{listingData.details}</p>
           <button
-            onClick={() => {
-              if (window.ethereum && window.web3) {
-                window.ethereum.enable().then(accounts => {
-                  const web3 = new Web3(window.web3.currentProvider);
-                  const contract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
-                  contract.methods
-                    .createOrder(
-                      listingID,
-                      1,
-                      CONTRACT_ADDRESS,
-                      "test@test.com"
-                    )
-                    .send({
-                      from: accounts[0],
-                      to: "",
-                      value: listingData.price,
-                      gasPrice: "20000000000"
-                    })
-                    .then(response => console.log(response));
-                }).catch(error => {
-                  console.log(error)
-                })
-              }
-            }}
+            onClick={() =>
+              makePurchase(listingID, listingData.price, 1, "test@test.com")
+            }
           >
             Buy Now
           </button>
