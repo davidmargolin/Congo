d78d756c0bfc29bd94bbbba48bc30e03144965a0
David Margolin <davidmargolin0305@gmail.com>
2019-12-09 20:52:42 -0500

    clean up styles again

--- frontend/src/pages/ListingPage/index.jsx
+++ frontend/src/pages/ListingPage/index.jsx
@@ -21,98 +21,88 @@ const ListingPage = () => {
 
   if (!listingData) return <div></div>;
   return (
-    <span
+    <div
       style={{
+        flexDirection: "row",
         display: "flex",
+        flexWrap: "wrap",
         flex: 1,
-        alignItems: "center",
-        flexDirection: "column"
+        justifyContent: "center"
       }}
     >
-      <div
-        style={{
-          flexDirection: "row",
-          display: "flex",
-          flexWrap: "wrap",
-          maxWidth: 1500,
-          padding: 24,
-          justifyContent: "flex-start"
-        }}
-      >
-        <img
-          src={listingData.imageLink}
-          style={{ width: 400, height: 400, objectFit: "contain" }}
-        />
-        <br />
-        <div style={{ flex: 1, padding: 24 }}>
-          <h3>{listingData.name}</h3>
-          <p>{listingData.price / 1000000000000000000} ETH</p>
-          <a href={`https://ropsten.etherscan.io/address/${listingData.owner}`}>
-            View Seller Info
-          </a>
-          <p>{listingData.details}</p>
-          <form
-            style={{
-              display: "flex",
-              flex: 1,
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-              maxWidth: 800,
-              minHeight: 200
-            }}
-            onSubmit={event => {
-              event.preventDefault();
+      <img
+        src={listingData.imageLink}
+        style={{ padding: 12, width: 400, height: 400, objectFit: "contain" }}
+      />
+      <div style={{ padding: 12, maxWidth: 800 }}>
+        <h3>{listingData.name}</h3>
+        <p>{listingData.price / 1000000000000000000} ETH</p>
+        <a href={`https://ropsten.etherscan.io/address/${listingData.owner}`}>
+          View Seller Info
+        </a>
+        <p>{listingData.details}</p>
+        <form
+          style={{
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            maxWidth: 800,
+            minHeight: 200
+          }}
+          onSubmit={event => {
+            event.preventDefault();
 
-              methods
-                .createOrder(listingID, 1, "", email.current.value)
-                .send({
-                  from: chosenAccount,
-                  value: listingData.price
-                })
-                .then(response => console.log(response));
-            }}
-          >
-            <span>
-              It is recommended to use a burner email and not your personal
-              email. You can find one{" "}
-              <a
-                href="https://temp-mail.org/en/"
-                rel="noopener noreferrer"
-                target="_blank"
-                style={{
-                  textDecoration: "underline",
-                  color: "blue"
-                }}
-              >
-                here.
-              </a>
-            </span>
-            <input
-              id="buyerEmail"
-              type="email"
-              ref={email}
-              placeholder="Your email address..."
-              required
-            />
+            methods
+              .createOrder(listingID, 1, "", email.current.value)
+              .send({
+                from: chosenAccount,
+                value: listingData.price
+              })
+              .then(response => console.log(response));
+          }}
+        >
+          <span>
+            It is recommended to use a burner email and not your personal email.
+            You can find one{" "}
+            <a
+              href="https://temp-mail.org/en/"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{
+                textDecoration: "underline",
+                color: "blue"
+              }}
+            >
+              here.
+            </a>
+          </span>
 
-            <div style={{ justifyContent: "flex-end", display: "flex" }}>
-              <button
-                style={{
-                  color: "rbg(0, 0, 0)",
-                  fontSize: 16,
-                  backgroundColor: "#f9de9f",
-                  padding: 6,
-                  border: "1px solid black"
-                }}
-                type="submit"
-              >
-                Buy Now
-              </button>
-            </div>
-          </form>
-        </div>
+
+          <div style={{ justifyContent: "flex-end", display: "flex" }}>
+          <input
+            id="buyerEmail"
+            type="email"
+            ref={email}
+            placeholder="Your email address..."
+            required
+          />
+            <button
+              style={{
+                color: "rbg(0, 0, 0)",
+                fontSize: 16,
+                backgroundColor: "#f9de9f",
+                padding: 6,
+                border: "1px solid black"
+              }}
+              type="submit"
+            >
+              Buy Now
+            </button>
+          </div>
+        </form>
       </div>
-    </span>
+    </div>
   );
 };
 
